ec46ad4708f8fb521f2a07c22ce012e2153bd61c
chkim-glosori <kch4731@glosori.com>
2024-12-09 14:44:10 +0900

    게시물 Entity 추가

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,12 +4,23 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model): # Inheritance
+
+    # List : choices
+    # Key : labels
+    # Value : values
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250) # -> VARCHAR : 일반적으로 사용. 띄어쓰기 가능함.
     slug = models.SlugField(max_length=250) # -> VARCHAR : 소문자 + 하이픈(-)을 사용하는 게 컨벤션. URL 등에 적격
     body = models.TextField() # TEXT
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜가 자동으로 저장된다.
     updated = models.DateTimeField(auto_now=True) # 객체를 저장할 때 날짜가 자동으로 저장된다.
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta: # 모델 내부에 Meta 클래스를 추가하여, 모델에 대한 메타데이터를 정의함.
         ordering = ['-publish'] # 출시일 역순으로 정렬
