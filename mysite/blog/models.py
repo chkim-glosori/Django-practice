8ccfb000ddbac578b87a2e45f60297d59dca1aa6
chkim-glosori <kch4731@glosori.com>
2024-12-09 11:33:15 +0900

    feat: Blog Entity 추가

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model): # Inheritance
+    title = models.CharField(max_length=250) # -> VARCHAR : 일반적으로 사용. 띄어쓰기 가능함.
+    slug = models.SlugField(max_length=250) # -> VARCHAR : 소문자 + 하이픈(-)을 사용하는 게 컨벤션. URL 등에 적격
+    body = models.TextField() # TEXT
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜가 자동으로 저장된다.
+    updated = models.DateTimeField(auto_now=True) # 객체를 저장할 때 날짜가 자동으로 저장된다.
+
+    class Meta: # 모델 내부에 Meta 클래스를 추가하여, 모델에 대한 메타데이터를 정의함.
+        ordering = ['-publish'] # 출시일 역순으로 정렬
+        indexes = [
+            models.Index(fields=['-publish']), # 인덱스를 출시일 역순으로
+        ]
+
+    def __str__(self):
+        return self.title
