b329b9a1099e3fa7bc28c03fcbfa1b3b40bf2231
chkim-glosori <kch4731@glosori.com>
2024-12-13 10:50:20 +0900

    Pagination 적용

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all() # 커스텀 관리자
+    post_list = Post.published.all() # 커스텀 관리자
+    # 페이지 당 세 개의 게시물로 페이지를 매김.
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
