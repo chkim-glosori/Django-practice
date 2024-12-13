a01165762a818b732e502f6bac51cf177ef81ae5
chkim-glosori <kch4731@glosori.com>
2024-12-13 14:35:00 +0900

    클래스 기반 View 전환에 따른 코드 변경

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 
-def post_list(request):
-    post_list = Post.published.all() # 커스텀 관리자
-    # 페이지 당 세 개의 게시물로 페이지를 매김.
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    '''
+    Alternative post list view
+    '''
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -19,10 +18,6 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
-    
-    # get_object_or_404 메서드가 아닌 get() 메서드를 사용한 할 경우의 예외 처리
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
     
     return render(request,
                   'blog/post/detail.html',
